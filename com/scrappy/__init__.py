1f7af32b9c17039e261eec5d1f59321815907a4b
Arpnik Singh <s.arpniksingh1997@gmail.com>
2021-12-18 17:07:53 +0530

    refactored code

---
+++ com/scrappy/__init__.py
@@ -0,0 +1,8 @@
+def main():
+    print("Welcome To Drappy (Drop-Box Scraper)")
+    scrapingUrl = input("Enter the url to be scraped: ")
+    password = input("Enter the password to Access the DropBox: ")
+    folderAddr = input("Enter the folder address to store files: ")
+    print(scrapingUrl+" "+password+" "+ folderAddr)
+
+main()
